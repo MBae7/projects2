5ce58cf87dde31b5776e391bf13e1186183e41ef
MBae7 <143737119+MBae7@users.noreply.github.com>
2025-09-03 11:15:29 -0700

    attention

--- unit2.py
+++ unit2.py
@@ -10,5 +10,20 @@ def testGreeting(name,expected) :
         print("FAIL "+greeting(name))
         return False 
     
-testGreeting("Mia", "Hello, Mia, how are you?") 
-testGreeting("Dr. Seuss", "Hello, Dr. Seuss, how are you?")   
+#testGreeting("Mia", "Hello, Mia, how are you?") testGreeting("Dr. Seuss", "Hello, Dr. Seuss, how are you?")   
+
+def attention(string) :
+    cut =string[0:8]
+    if cut =="Hey you!":
+        return True
+    else :
+        return False
+
+def testAttention(string, expected) :
+    if attention(string) == expected :
+        print("Success! "+string) 
+    else :
+        print("FAIL "+string[0:8])
+        
+testAttention("Hey you! Come here", True)
+testAttention("Hi! I'm a frog", False)
